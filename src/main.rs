11bede3a122a8ac961b1c1a8ef11beb1c090af04
liztaliah <liz.t.hardee@gmail.com>
2024-10-30 15:15:40 -0700

    need to fix list method

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
 use rusqlite::{Connection, Result};
 
 #[derive(Debug)]
-struct Item {
+struct Items {
     file_name: String,
 }
 
-impl Item {
+#[derive(Debug)]
+struct Item {
+    number: i32,
+    name: String,
+    completed: i32,
+}
+
+impl Items {
     pub fn add(&self, name: &str, completed: i32) {
         let connection = Connection::open(&self.file_name).unwrap();
         connection.execute("insert into items (name, completed) values (?1, ?2)",
             (name, completed)).unwrap();
     }
+
+    pub fn list(&self) -> Result<()> {
+        let connection = Connection::open(&self.file_name).unwrap();
+        let mut statement = connection.prepare(
+            "select number, name, completed from items"
+        )?;
+        let item_iter = statement.query_map([], |row| {
+            Ok(Item {
+                number: row.get(0)?,
+                name: row.get(1)?,
+                completed: row.get(2)?,
+            })
+        })?;
+
+        for item in item_iter {
+            println!("Found Item {:?}", item.unwrap());
+        }
+        Ok(())
+    }
 }
 
 fn main() {
-    let db = Item {
+    let db = Items {
         file_name: String::from("./data/data.db"),
     };
 
-    db.add("pair", 0);
+    db.add("orange", 0);
+    db.list();
 }
